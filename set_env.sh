53076224e609de1e5a77fc87c89670c599b30b2e
huy <quanghuy56xf@gmail.com>
2022-12-07 14:45:34 +0700

    fist commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=huy
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=huy.cdteves2zrkf.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=test-992654605303-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=hello
 export URL=http://localhost:8100
